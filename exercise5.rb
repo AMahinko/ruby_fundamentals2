3e321707c00bb7674a2ea001b8abe8797b430f06
Alex <alex.mahinko@gmail.com>
2017-03-28 14:28:22 -0400

    Fixed index bug

--- exercise5.rb
+++ exercise5.rb
@@ -1,15 +1,21 @@
 def backwards_name(name)
   output=""
   letter_buffer = []
-  letter_selector = name.length
+  letter_selector = name.length + 1
+
+  puts "letter selector is #{letter_selector}"
+  puts "buffer is #{letter_buffer}"
 
   while letter_selector > 0
     letter_buffer << name.slice!(letter_selector)
+    puts "letter selector is #{letter_selector}"
+    puts "buffer is #{letter_buffer}"
     letter_selector -= 1
   end
 
   letter_buffer.each do |letter|
     output = output + letter_buffer[-1]
+    puts "output is #{output}"
   end
 
   return output
