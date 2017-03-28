f51e8ff9a27da2077f289351f9707cf9d42c6063
Alex <alex.mahinko@gmail.com>
2017-03-28 14:35:07 -0400

    Buffer now serves up letters to function output properly. Testing and moving on

--- exercise5.rb
+++ exercise5.rb
@@ -1,20 +1,22 @@
 def backwards_name(name)
   output=""
   letter_buffer = []
-  letter_selector = name.length + 1
+  letter_selector = name.length - 1
+  name_length = name.length
 
   puts "letter selector is #{letter_selector}"
   puts "buffer is #{letter_buffer}"
 
-  while letter_selector > 0
+  while letter_selector > -1
     letter_buffer << name.slice!(letter_selector)
     puts "letter selector is #{letter_selector}"
     puts "buffer is #{letter_buffer}"
     letter_selector -= 1
   end
 
-  letter_buffer.each do |letter|
-    output = output + letter_buffer[-1]
+  name_length.times do |letter|
+    output = output + letter_buffer[0]
+    letter_buffer.slice!(0)
     puts "output is #{output}"
   end
 
@@ -22,3 +24,6 @@ def backwards_name(name)
 end
 
 puts backwards_name("Jack")
+puts backwards_name("Zimi")
+puts backwards_name("Hakkon")
+puts backwards_name("Xerxes")
