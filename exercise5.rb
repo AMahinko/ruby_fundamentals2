d0c985402f6ee0f44070a7bef4cead20da4224de
Alex <alex.mahinko@gmail.com>
2017-03-28 14:38:45 -0400

    Tested and removed debug alerts.

--- exercise5.rb
+++ exercise5.rb
@@ -4,20 +4,15 @@ def backwards_name(name)
   letter_selector = name.length - 1
   name_length = name.length
 
-  puts "letter selector is #{letter_selector}"
-  puts "buffer is #{letter_buffer}"
 
   while letter_selector > -1
     letter_buffer << name.slice!(letter_selector)
-    puts "letter selector is #{letter_selector}"
-    puts "buffer is #{letter_buffer}"
     letter_selector -= 1
   end
 
   name_length.times do |letter|
     output = output + letter_buffer[0]
     letter_buffer.slice!(0)
-    puts "output is #{output}"
   end
 
   return output
