5ae4c41a41ad1bdaa3b48d4d4c7489027d2000a5
Alex <alex.mahinko@gmail.com>
2017-03-28 14:23:50 -0400

    Trying using slices instead to populate letter buffer array

--- exercise5.rb
+++ exercise5.rb
@@ -1,8 +1,18 @@
 def backwards_name(name)
+  output=""
+  letter_buffer = []
+  letter_selector = name.length
 
-name.each_char { |letter|
-letter_buffer = [] << letter
-output << letter_buffer
-return output  }
+  while letter_selector > 0
+    letter_buffer << name.slice!(letter_selector)
+    letter_selector -= 1
+  end
 
+  letter_buffer.each do |letter|
+    output = output + letter_buffer[-1]
+  end
+
+  return output
 end
+
+puts backwards_name("Jack")
