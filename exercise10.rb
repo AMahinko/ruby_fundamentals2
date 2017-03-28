624cd3078b43c56b4105c60b15f849c89a42ead0
Alex <alex.mahinko@gmail.com>
2017-03-28 17:16:48 -0400

    Initialized and defined listing variable

--- exercise10.rb
+++ exercise10.rb
@@ -3,3 +3,16 @@ students = {
   cohort2: 42,
   cohort3: 22
 }
+
+def cohort_printout(list)
+
+  list.each do |key, value|
+
+    puts key.to_s + ": " + value.to_s
+
+  end
+
+end
+
+
+cohort_printout(students)
