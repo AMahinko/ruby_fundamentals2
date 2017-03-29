fbd47a6b810280e6c6fe36b0b34382b13e898759
Alex <alex.mahinko@gmail.com>
2017-03-29 00:37:55 -0400

    That's enough stretch goals

--- exercise12.rb
+++ exercise12.rb
@@ -4,6 +4,27 @@ my_dogs = [
   {name: "Jade", position: 11}
 ]
 
-test1 = my_dogs.select {|hash, key, value| hash[:position] > 10}
+my_dogs.map
+
+
+def get_absent_dogs(array)
+  absent_dogs = []
+  callback = []
+
+
+absent_dogs = my_dogs.select {|hash, key, value| hash[:position] > 10}
+
+for dog in absent_dogs
+  dog.each do |key, value|
+    callback << "Come back, " + value.to_s + "!" if value.is_a?(String) == true
+  end
+end
+
+  return callback
+
+end
+
+puts get_absent_dogs(my_dogs)
+
 
 puts test1
